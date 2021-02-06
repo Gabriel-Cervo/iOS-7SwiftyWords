d44c740cad7bea5513143883a15f0c06f2ef420b
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-06 07:10:55 -0300

    ADD metodo para carregar arquivo e assim, o nivel

--- 100DaysOfSwift-Project8/ViewController.swift
+++ 100DaysOfSwift-Project8/ViewController.swift
@@ -156,6 +156,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        guard let levelFileUrl = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") else {
+            print("No file found")
+            return
+        }
+        
+        guard let levelContents = try? String(contentsOf: levelFileUrl) else {
+            print("Cant convert file to string")
+            return
+        }
+        
+        var lines = levelContents.components(separatedBy: "\n")
+        lines.shuffle()
+        
+        // Looping com index
+        for (index, line) in lines.enumerated() {
+            let parts = line.components(separatedBy: ": ")
+            let answer = parts[0]
+            let clue = parts[1]
+            
+            clueString += "\(index + 1). \(clue)\n"
+            
+            let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+            solutionString += "\(solutionWord.count) letters \n"
+            solutions.append(solutionWord)
+            
+            
+            let bits = answer.components(separatedBy: "|")
+            letterBits += bits
+        }
+    }
+    
     @objc func letterTapped(_ sender: UIButton) {
     }
 
